c5ad9c5b3e3854e0b0d465a34f83ec06f4642052
Dong Jun <121388798+Chan-Dong-Jun@users.noreply.github.com>
2024-02-17 21:51:29 +0800

    initial commit

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import InputField from "./components/input.tsx";
+function App() {
+  
+
+  return (
+    <>
+      <InputField></InputField>
+    </>
+  )
+}
+
+export default App
---
+++ my-first-react-app/src/components/input.tsx
@@ -0,0 +1,8 @@
+export default InputField = () => {
+    return (
+        <>
+            <span>test</span>
+        </>
+    );
+};
+
