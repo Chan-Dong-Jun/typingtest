2abe0d183667f3c0cbe0806e782da1da9810c7df
Dong Jun <121388798+Chan-Dong-Jun@users.noreply.github.com>
2024-02-25 10:37:16 +0800

    inputtext

--- webpage/src/InputComponent/input.tsx
+++ webpage/src/InputComponent/input.tsx
@@ -6,14 +6,50 @@ const InputField = () => {
     const isAlphaNumeric = str => /^[a-z0-9 ]+$/i.test(str);
 
     let correctText = "It as announcing it me stimulated frequently continuing. Least their she you now above going stand forth. He pretty future afraid should genius spirit on. Set property addition building put likewise get. Of will at sell well at as. Too want but tall nay like old. Removing yourself be in answered he. Consider occasion get improved him she eat. Letter by lively oh denote and"
-    correctText =  correctText.split(" ").map(e=>e.split(""));
+    //correctText = correctText.split(" ").map(e=>e.split(""));
+    correctText = correctText.split(" ").map(e => [...e, " "]);
+
     const [inputText, setInputText] = useState(Array(correctText.length).fill([]));
     const [index, setIndex] = useState(0);
-    console.log(index)
-    console.log(correctText)
-    console.log(inputText)
+    // console.log(index)
+    // console.log(correctText)
+    // console.log(inputText)
     const ref = useRef(null)
-  
+    
+    // const renderTextWithStyle = () => {
+    //     console.log(inputText)
+    //     return inputText.map((word, indexOuter) => {
+    //         return word.map((char, indexInner) => {
+    //             let charColour;
+    //             if (indexOuter >= correctText.length || indexInner >= correctText[indexOuter].length) {
+    //                 charColour = "red";
+    //             } else if (correctText[indexOuter][indexInner] == char) {
+    //                 charColour = "green";
+    //             } else {
+    //                 charColour = "red";
+    //             }
+    //             return <span key={index} style={{ color: charColour }}>{char}</span>;
+    //         }) 
+    //     })
+    // }
+    const renderTextWithStyle = () => {
+        return inputText.map((word, indexOuter) => {
+            return word.map((char, indexInner) => {
+                let charColour;
+                if (indexOuter >= correctText.length || indexInner >= correctText[indexOuter].length) {
+                    charColour = "red";
+                } else if (correctText[indexOuter][indexInner] === char) {
+                    charColour = "green";
+                } else {
+                    charColour = "red";
+                }
+                // Generate a unique key for each span element
+                const key = `${indexOuter}-${indexInner}`;
+                return <span key={key} style={{ color: charColour }}>{char}</span>;
+            }) 
+        })
+    }
+    
 
     const handleKeyDown = event => {
         console.log('User pressed: ', event.key);
@@ -48,33 +84,35 @@ const InputField = () => {
 
         if (event.key == " ") {    
             if (index < correctText.length - 1) {
-                setIndex(index => index + 1)
+                const newInputText = inputText.map((word,i) => {
+                    if (index == i) {
+                        return [...word, " "]
+                    } else {
+                        return word;
+                    }
+                })
+                setInputText(newInputText);
+                setIndex(index => index + 1);
             }
         }
-        
     };
 
     useEffect(() => {
         ref.current.focus();
       }, []);
 
-    // const renderTextWithStyle = () => {
-    //     for inputText[:index+1].map((word, index)
 
-    //     )
-    // }
-    
     return (
         <>
             <div 
-            ref={ref} 
-            className="displayText" 
+            ref={ref}  
             onKeyDown={handleKeyDown}
             tabIndex={-1}>
-                {inputText.map((e) => {
+                {renderTextWithStyle()}
+                {/* {inputText.map((e) => {
                     return e.join("")
                 }
-                    ).join(" ")}
+                    ).join(" ")} */}
             </div>
         </>
     );
